507d6335a6251f8bcd97d4cd1c275d2621520485
osakh <saharukoleg1@gmail.com>
2019-11-13 15:04:01 +0200

    add OneToMany mapping for field CartItemList In CartItem class to field product (mappedBy = "product") it mean that CartItemList may CONTAIN many products

--- src/main/java/com/emusicstore/model/Product.java
+++ src/main/java/com/emusicstore/model/Product.java
@@ -5,26 +5,40 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
 import javax.validation.constraints.Min;
+import java.io.Serializable;
+import java.util.List;
 
 @Entity
-public class Product {
+public class Product implements Serializable {
+
+
+    private static final long serialVersionUID = -3532377236419382983L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private String productId;
+
+
     @NotEmpty(message = "The product name must not be null.")
     private String productName;
     private String productCategory;
     private String productDescription;
+
+
     @Min(value = 0, message = "The product price must not be less then zero")
     private double productPrice;
     private String productCondition;
     private String productStatus;
+
+
     @Min(value = 0, message = "The product unit price must not be less then zero")
     private int unitInStock;
     private String productManufacturer;
 
     @Transient
     private MultipartFile productImage;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<CartItem> cartItemList;
 
 
     public String getProductId() {
